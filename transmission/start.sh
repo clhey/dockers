18ad8eca3d731b8acbf66d5dcf91b49326f4e1f6
黑承亮 <heichengliang@aixuedai.com>
2016-07-21 17:53:07 +0800

    添加aria2和transmission

---
+++ transmission/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /config
+
+if [ ! -f /config/settings.json ]; then
+    mv /etc/transmission-daemon/settings.json /config/settings.json
+    rm /var/lib/transmission-daemon/info/settings.json
+else
+    rm /var/lib/transmission-daemon/info/settings.json
+    rm /etc/transmission-daemon/settings.json
+fi
+
+ln -sf /config/settings.json /var/lib/transmission-daemon/info/settings.json
+ln -sf /config/settings.json /etc/transmission-daemon/settings.json
+
+/usr/bin/transmission-daemon --foreground --config-dir /config --log-info --username ${USERNAME} --peerport 51413 --password ${PASSWORD} --auth --watch-dir /watch --download-dir /downloads --incomplete-dir /incomplete
